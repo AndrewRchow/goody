af86f21ba019fdd01efbdf8c8c64386d423c4f9f
idrivediesel2006_sabio_laptop <victor@sabio.la>
2017-04-27 12:45:52 -0700

    Added CKEditor js reference to Index Page

---
+++ Web/Scripts/goody.js
@@ -0,0 +1,44 @@
+﻿var goody = {
+    utilities: {}
+    , layout: {
+        preStart: null,
+        postStart: null
+    }
+    , page: {
+        handlers: {
+        }
+        , startUp: null
+    }
+    , services: {}
+    , ui: {}
+
+};
+goody.moduleOptions = {
+    APPNAME: "goodyApp"
+        , extraModuleDependencies: []
+        , runners: []
+        , page: goody.page//we need this object here for later use
+};
+
+goody.layout.startUp = function () {
+
+    console.debug("goody.layout.startUp");
+    if (goody.layout.preStart) {
+        console.debug("goody.layout.preStart");
+        goody.layout.preStart();
+    }
+    //this does a null check on sabio.page.startUp
+    if (goody.page.startUp) {
+        console.debug("goody.page.startUp");
+        goody.page.startUp();
+    }
+    if (goody.layout.postStart) {
+        console.debug("goody.layout.postStart");
+        goody.layout.postStart();
+    }
+};
+goody.APPNAME = "goodyApp";//legacy
+
+
+
+$(document).ready(goody.layout.startUp);
