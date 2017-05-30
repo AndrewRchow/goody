f811cd6da024c42d6b92abccf20f825392b5b9d2
idrivediesel2006_sabio_laptop <victor@sabio.la>
2017-05-30 15:36:59 -0700

    Finished Angularizing CD Editor

---
+++ Web/Controllers/EditorNgController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Goody.Web.Controllers
+{
+    public class EditorNgController : Controller
+    {
+        // GET: EditorNg
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
