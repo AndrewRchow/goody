d175c7e3d35a29e974696e79f9573c2b13d41827
idrivediesel2006_sabio_laptop <victor@sabio.la>
2017-07-18 16:01:15 -0700

    Checking in CDN stuff

--- Web/App_Start/BundleConfig.cs
+++ Web/App_Start/BundleConfig.cs
@@ -8,24 +8,39 @@ namespace Goody.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.UseCdn = true;
+            bundles.Add(new ScriptBundle("~/bundles/jquery",
+                "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js")
+                .Include("~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval",
+                "https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.min.js")
+                .Include("~/Scripts/jquery.validate.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryvalunobtrusive",
+                "https://cdnjs.cloudflare.com/ajax/libs/jquery-validation-unobtrusive/3.2.6/jquery.validate.unobtrusive.min.js")
+                .Include("~/Scripts/jquery.validate.unobtrusive.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr",
+                "https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js")
+                .Include("~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap",
+                "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js")
+                .Include("~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/respond",
+                "https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js")
+                .Include("~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css",
+                "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css")
+                .Include("~/Content/bootstrap.css"));
+
+            bundles.Add(new StyleBundle("~/Content/sitecss")
+                .Include("~/Content/site.css"));
         }
     }
 }
