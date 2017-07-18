e33522d6c6194ee9284b5109c633c62cde6e870e
idrivediesel2006_sabio_laptop <victor@sabio.la>
2017-07-18 15:49:25 -0700

    interim checkin to adjust cdn stuff

--- Web/Controllers/EditorNgController.cs
+++ Web/Controllers/EditorNgController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Goody.Web.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,22 @@ using System.Web.Mvc;
 
 namespace Goody.Web.Controllers
 {
+    [RoutePrefix("editorng")]
     public class EditorNgController : Controller
     {
         // GET: EditorNg
         public ActionResult Index() 
         {
             return View();
+        }
+
+        [Route("{id:int}/manage")]
+        [Route("create")]
+        public ActionResult Manage(int id = 0)
+        {
+            ItemViewModel<int> model = new ItemViewModel<int>();
+            model.Item = id;
+            return View(model);
         }
     }
 }
