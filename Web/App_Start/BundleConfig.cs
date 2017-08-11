c9fc4bc5fdbd280c8011dda330ece8f75d72b38e
idrivediesel2006_sabio_laptop <victor@sabio.la>
2017-08-10 19:22:15 -0700

    added angular bundles and cdn references

--- Web/App_Start/BundleConfig.cs
+++ Web/App_Start/BundleConfig.cs
@@ -35,10 +35,71 @@ namespace Goody.Web
                 "https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js")
                 .Include("~/Scripts/respond.js"));
 
-            // Angular stuff
+            // Angular Animate
+            bundles.Add(new ScriptBundle("~/bundles/angular-animate",
+                "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular-animate.min.js")
+                .Include("~/Scripts/angularjs/angular-animate.js"));
+
+            // Angular Aria
+            bundles.Add(new ScriptBundle("~/bundles/angular-aria",
+                "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular-aria.min.js")
+                .Include("~/Scripts/angularjs/angular-aria.js"));
+
+            // Angular Cookies
+            bundles.Add(new ScriptBundle("~/bundles/angular-cookies",
+                "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular-cookies.min.js")
+                .Include("~/Scripts/angularjs/angular-cookies.js"));
+
+            // Angular Loader
+            bundles.Add(new ScriptBundle("~/bundles/angular-loader",
+                "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular-loader.min.js")
+                .Include("~/Scripts/angularjs/angular-loader.js"));
+
+            // Angular Message Format
+            bundles.Add(new ScriptBundle("~/bundles/angular-message-format",
+                "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular-message-format.min.js")
+                .Include("~/Scripts/angularjs/angular-message-format.js"));
+
+            // Angular Message
+            bundles.Add(new ScriptBundle("~/bundles/angular-messages",
+                "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular-messages.min.js")
+                .Include("~/Scripts/angularjs/angular-messages.js"));
+
+            // Angular Parse
+            bundles.Add(new ScriptBundle("~/bundles/angular-parse-ext",
+                "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular-parse-ext.min.js")
+                .Include("~/Scripts/angularjs/angular-parse-ext.js"));
+
+            // Angular Resource
+            bundles.Add(new ScriptBundle("~/bundles/angular-resource",
+                "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular-resource.min.js")
+                .Include("~/Scripts/angularjs/angular-resource.js"));
+
+            // Angular Route
+            bundles.Add(new ScriptBundle("~/bundles/angular-route",
+                "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular-route.min.js")
+                .Include("~/Scripts/angularjs/angular-route.js"));
+
+            // Angular Sanitize
+            bundles.Add(new ScriptBundle("~/bundles/angular-sanitize",
+                "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular-sanitize.min.js")
+                .Include("~/Scripts/angularjs/angular-sanitize.js"));
+
+            // Angular Touch
+            bundles.Add(new ScriptBundle("~/bundles/angular-touch",
+                "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular-touch.min.js")
+                .Include("~/Scripts/angularjs/angular-touch.js"));
+
+            // Angular JS
             bundles.Add(new ScriptBundle("~/bundles/angular",
                 "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular.min.js")
-                .Include("~/Scripts/angular.min.js"));
+                .Include("~/Scripts/angularjs/angular.js"));
+
+            // Angular JS CSS
+            bundles.Add(new StyleBundle("~/Content/angularjs-css",
+                "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular-csp.css")
+                .Include("~/Content/angularjs/angular-csp.css"));
+
 
             bundles.Add(new StyleBundle("~/Content/css",
                 "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css")
